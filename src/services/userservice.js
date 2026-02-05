7746f2e91a4ad1c649139eb0cbb93a9d141358d5
abhiverma33397-sudo <abhiverma33397@gmail.com>
2026-02-05 14:52:08 +0530

    Fix API endpoints for user authentication services

--- src/services/userservice.js
+++ src/services/userservice.js
@@ -2,16 +2,16 @@ import baseURL from "./baseurl";
 
 
 
-export const login = (values) => baseURL.post("Auth/login", values);
+export const login = (values) => baseURL.post("UserLogin/login", values);
 export const signUp = (values) => baseURL.post("User/register", values);
 export const forgotPassword = (values) => {
 	const payload = { email: values?.Email || values?.email };
 	console.log(payload);
 	return baseURL.post("Auth/forgot-password", payload);
 };
-export const verifyOTP = (values) => baseURL.post("Auth/verify-otp", values);
+export const verifyOTP = (values) => baseURL.post("UserLogin/verify-otp", values);
 
-export const resetPassword = (values) => baseURL.post("Auth/reset-password", values);
+export const resetPassword = (values) => baseURL.post("UserLogin/reset-password", values);
 export const changePassword = (values) => baseURL.post("Auth/change-password", values);
 
 
